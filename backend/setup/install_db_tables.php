09ccbac7e9455a2071d67f821a711473ea79b446
Arnold Noronha <arnold@arnosh.(none)>
2009-06-01 01:08:33 -0400

    Updated Makefile to run "make database" automatically.

--- backend/setup/install_db_tables.php
+++ backend/setup/install_db_tables.php
@@ -5,8 +5,10 @@ require_once "../config.inc" ;
 require_once "lib/db.inc" ;
 
 $sql = file_get_contents("../database") ;
-echo $sql ;
 
-contestDB::get_zend_db()->getConnection()->exec("$sql");
-
+try {
+	contestDB::get_zend_db()->getConnection()->exec("$sql");
+} catch (Exception $e) {
+	echo "Notice: database tables are already installed from previous make\n";
+}
 
