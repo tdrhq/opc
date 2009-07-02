317336794a72f3c09146d4d168ac3cde3fe79ef7
Arnold Noronha <arnold@arnosh.(none)>
2009-07-02 17:07:37 -0400

    Only DB_Name is required in case of Pdo_Sqlite.

--- backend/setup/install_database.php
+++ backend/setup/install_database.php
@@ -6,10 +6,17 @@ if ( $argv[1] == "--check" ) {
   /* check to see if a database is already in place */ 
   require_once "../config.inc" ; 
   require_once "lib/db.inc" ;
-  if ( config::$DB_Name == "" or config::$DB_Password == "" or
+  if (strcasecmp(config::$DB_Adapter, "Pdo_Sqlite") != 0) {
+    if ( config::$DB_Name == "" or config::$DB_Password == "" or
 	   config::$DB_User == "" or config::$DB_Hostname == "" ) { 
-	echo ("You have not provided complete database information. Edit ./local_config.inc\n") ;
-	exit(1) ;
+  	echo ("You have not provided complete database information. Edit ./local_config.inc\n") ;
+ 	exit(1) ;
+    }
+  } else {
+	if (empty(config::$DB_Name)) {
+	  	echo "Sqlite db name not provided. Edit ./local_config.inc\n";
+		exit (1);
+	}
   }
 
   /* the next function call will cause the script to die with 0 return value
