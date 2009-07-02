ea6bc24876214f2a9eafd72ef07009349d99a622
Arnold Noronha <arnold@arnosh.(none)>
2009-07-02 12:32:41 -0400

    Moved around the schema files.

--- backend/setup/install_db_tables.php
+++ backend/setup/install_db_tables.php
@@ -4,7 +4,7 @@ chdir(dirname(__FILE__));
 require_once "../config.inc" ;
 require_once "lib/db.inc" ;
 
-$sql = file_get_contents("../database") ;
+$sql = file_get_contents("./schema.pg") ;
 
 try {
 	contestDB::get_zend_db()->getConnection()->exec("$sql");
