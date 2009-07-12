a0f13ac13bcf182f2a3793e91bd50c2dd7f1d191
Arnold Noronha <arnold@arnosh.(none)>
2009-07-12 01:06:25 -0400

    forgot-password.

--- webfiles/application/controllers/ForgotPasswordController.php
+++ webfiles/application/controllers/ForgotPasswordController.php
@@ -18,12 +18,6 @@ class ForgotPasswordController extends Zend_Controller_Action
 		return $str;
 	}
 	
-	function testPasswordRecovery ($user, $key) 
-	{
-		$savedkey = $cache->get ($user);
-		if ($savedkey != $key) return false;
-		else return true;
-	}
 
 	function preDispatch ()
 	{
@@ -50,11 +44,69 @@ class ForgotPasswordController extends Zend_Controller_Action
 
 		/* send email */
 		$mail = Mailer::get_mailer ();
-		$mail->setBodyText ($key);
+		$baseurl = Zend_Controller_Front::getInstance()->getBaseUrl();
+
+		if ($baseurl == "/") $baseurl = "";
+		$mail->setBodyText ("
+
+You are receiving this email because you wished to reset your password
+on http://{$_SERVER['SERVER_NAME']}$baseurl.
+
+Please use the following link to reset your password:
+   http://{$_SERVER['SERVER_NAME']}$baseurl/forgot-password/reset?user=$user&key=$key
+
+");
 		
 		$mail->addTo ($userm->getMember($obj, 0)->email,
 			      $userm->getMember($obj, 0)->name);
 		$mail->setSubject ("Password Reset");
 		$mail->send();
+	}
+
+	function resetAction ()
+	{
+		Zend_Loader::loadClass ("UserModel");
+		$userm = new UserModel ();
+		$user = $this->_request->get("user");
+		$key = $this->_request->get ("key");
+
+		$obj = $userm->getRow ($user);
+
+		if (empty($obj) or empty($key)) 
+			$this->_redirect ("/");
+
+		$savedkey = $this->cache->load ($user);
+
+		if ($savedkey != $key)
+			$this->_redirect ("/");
+
+		$this->cache->remove ($user);
+
+		$pass = "";
+		for ($i = 0; $i < 8; $i++) {
+			$pass = $pass . mt_rand (0, 9);
+		}
+
+		/* send email */
+		$mail = Mailer::get_mailer ();
+		$baseurl = Zend_Controller_Front::getInstance()->getBaseUrl();
+		if ($baseurl == "/") $baseurl = "";
+		$mail->setBodyText ("
+
+You are receiving this email because you wished to reset your password
+on http://{$_SERVER['SERVER_NAME']}$baseurl.
+
+We have changed your password to: $pass
+
+Please try logging in using this password and change it as soon as
+possible.
+
+");
+		$mail->addTo ($userm->getMember($obj, 0)->email,
+			      $userm->getMember($obj, 0)->name);
+		$mail->setSubject ("Password Reset");
+		$mail->send();
+		
+		$obj->setPassword ($pass);
 	}
 }
