9f8aea5048729c9e2e0effd7cc323355d6df7a9f
Arnold Noronha <arnold@arnosh.(none)>
2009-07-25 01:30:22 -0400

    Added a few stub tests. AllowRegsterTest for testing the $allow_register option.

---
+++ tests/AllowRegisterTest.php
@@ -0,0 +1,26 @@
+<?
+
+require_once "OpcTest.php";
+require_once  "lib/db.inc";
+
+class AllowRegisterTest extends OpcTest
+{
+	public function setUp ()
+	{
+		webconfig::$allow_register = false;
+		parent::setUp ();
+	}
+
+	public function testRegistrationFails() 
+	{
+		/* no exceptions, right? */
+		$this->request->setMethod("POST")->setPost(array('user' => 'dfsdfdfewwww_ewr', 'password' => 'simplepassword', 
+	 'confirm' => 'simplepassword', 'name0' => 'Arnold', 'email0' => 'arn@arn.com', 'timezone' => 'localtime'));
+		$this->dispatch("/profile/register2");
+		$this->assertController ("error");
+
+		/* todo: assertAction not working. What am I doing wrong? */
+		//$this->assertAction ("regnotavailable");
+	}
+
+}
