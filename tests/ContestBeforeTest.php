b3a2906e19dc3f8b473435626fbb628edfb14074
Arnold Noronha <arnold@arnosh.(none)>
2009-07-25 18:32:44 -0400

    more tests.

--- tests/ContestBeforeTest.php
+++ tests/ContestBeforeTest.php
@@ -4,20 +4,6 @@ require_once "OpcTest.php";
 require_once  "lib/db.inc";
 
 
-/* taken from SuController code */
-class SuAuthAdapter implements Zend_Auth_Adapter_Interface {
-        public function __construct($uid) {
-
-                        $this->result = new Zend_Auth_Result(
-                                Zend_Auth_Result::SUCCESS, $uid);
-                        return ;
-        }
-
-        public function authenticate() {
-                return $this->result ;
-        }
-
-}
 
 class ContestBeforeTest extends OpcTest
 {
@@ -36,11 +22,6 @@ class ContestBeforeTest extends OpcTest
 		webconfig::$multi_contest = true;
 
 		parent::setUp ();
-		/* "login" */
-		Zend_Loader::loadClass('Zend_Auth');
-		$adapter = new SuAuthAdapter ($test_nonadmin_uid1);
-		Zend_Auth::getInstance()->authenticate($adapter);
-
 	}
 
 	public function testContestCannotAccess() 
