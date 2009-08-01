164978b2d1100ef8869aa6f0b0a0ab3651e3e6af
Arnold Noronha <arnstein87@gmail.com>
2009-07-31 23:43:46 -0400

    fixed the ProblemsControllerTest.

--- tests/Tests/ProblemsControllerTest.php
+++ tests/Tests/ProblemsControllerTest.php
@@ -15,7 +15,6 @@ class ProblemsControllerTest extends OpcTest
 		global $general_contest_problem;
 		/* tricky, I don't know which problem to use at this point */
 		$db = contestDB::get_zend_db();
-		$obj = $res->fetchObject();
 		$this->dispatch ("/problems/$general_contest_problem");
 
 		$this->assertNotRedirect();
