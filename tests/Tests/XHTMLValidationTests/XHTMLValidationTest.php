00d924a29cc49208de88f4dc7697f42e713171b1
Arnold Noronha <arnstein87@gmail.com>
2009-10-10 14:24:40 -0400

    Fix XHTML issues with compile error status.

--- tests/Tests/XHTMLValidationTests/XHTMLValidationTest.php
+++ tests/Tests/XHTMLValidationTests/XHTMLValidationTest.php
@@ -29,9 +29,14 @@ class XHTMLValidationTest extends OpcDataTest
 		global $problems_for_validation;
 		global $test_nonadmin_uid1, $test_admin_uid;
 		global $submission_owned_by_uid1, $submission_owned_by_uid2;
+		global $test_submissions;
 
 		assert (!empty($problems_for_validation));
 		$ret = array ("/problems", "/queue", "/queue/index", "/submit", "/ranks", "/results/$submission_owned_by_uid1", "/results/$submission_owned_by_uid2");
+
+		foreach  ($test_submissions  as $i) {
+			$ret [] = "/results/{$i[0]}";
+		}
 		foreach ($problems_for_validation as $pr) 
 			$ret [] = "/problems/$pr";
 		$ret2 = array ();
