07dd070e9fc15e0871687285ee5d15ad68e96d80
Arnold Noronha <arnstein87@gmail.com>
2009-10-10 13:03:24 -0400

    CodeHighlight class required for previous few commits.

---
+++ webfiles/application/views/helpers/CodeHighlight.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "Zend/View/Helper/Abstract.php";
+require_once "/usr/share/php-geshi/geshi.php"; /* todo! replace path to geshi */
+
+class Zend_View_Helper_CodeHighlight
+	extends Zend_View_Helper_Abstract
+{
+	public function codeHighlight ($source, $lang)
+	{
+		if ($lang == "cpp") $lang = "C++";
+		if ($lang == "gcj") $lang = "Java";
+
+		$source = htmlentities ($source);
+		$geshi = new GeSHi ($source, $lang); 
+		$geshi->set_header_type(GESHI_HEADER_PRE_TABLE);
+		$geshi->enable_line_numbers (GESHI_NORMAL_LINE_NUMBERS);
+		$code = $geshi->parse_code ();
+		return $code;
+	}
+}
