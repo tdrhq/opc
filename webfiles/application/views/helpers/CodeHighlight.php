4588214f7853067cf65add2d0ae79fd9c9d9f22f
Arnold Noronha <arnstein87@gmail.com>
2009-10-10 13:51:42 -0400

    * remove htmlentities * add overall geshi class.

--- webfiles/application/views/helpers/CodeHighlight.php
+++ webfiles/application/views/helpers/CodeHighlight.php
@@ -11,10 +11,10 @@ class Zend_View_Helper_CodeHighlight
 		if ($lang == "cpp") $lang = "C++";
 		if ($lang == "gcj") $lang = "Java";
 
-		$source = htmlentities ($source);
 		$geshi = new GeSHi ($source, $lang); 
 		$geshi->set_header_type(GESHI_HEADER_PRE_TABLE);
 		$geshi->enable_line_numbers (GESHI_NORMAL_LINE_NUMBERS);
+		$geshi->set_overall_class ("geshi");
 		$code = $geshi->parse_code ();
 		return $code;
 	}
