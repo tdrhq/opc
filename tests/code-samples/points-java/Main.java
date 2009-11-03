73e576743f644e515bb7bbc8aaccb59595985441
Arnold Noronha <arnstein87@gmail.com>
2009-11-03 17:28:19 -0500

    sun java and with new test cases.

---
+++ tests/code-samples/points-java/Main.java
@@ -0,0 +1,8 @@
+
+class Main 
+{
+	public static void main (String[] arg)
+	{
+		System.out.println ("55\n58\n35\n60\n26\n42\n39\n44\n52\n52");
+	}
+}
---
+++ tests/code-samples/simple.java
@@ -0,0 +1,10 @@
+
+import java.io.*;
+
+class Main 
+{
+	public static void main (String[] arg)
+	{
+		System.out.println ("OK");
+	}
+}
