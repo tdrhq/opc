610ea8cacbd6c88bb11bf6a156e9cb3fd8769022
Arnold Noronha <arnstein87@gmail.com>
2010-01-20 21:44:28 -0500

    Ok clarified the previous commit. Still could do better understanding what that means. ;)

--- tests/OpcTest.php
+++ tests/OpcTest.php
@@ -11,12 +11,11 @@ require_once "../webfiles/App.php";
 abstract class OpcTest extends Zend_Test_PHPUnit_ControllerTestCase {
 	public function setUp ()
 	{
-		$bootstrap = new App ();
+		$app = new App ();
 
-		/* @todo: I have no idea what the following does. Need to 
-		 * find out and then add it in the comment or use more
-		 * simple logic */
-		$this->bootstrap = array ($bootstrap, 'bootstrap');
+		/* the following line is a Zend_Test or PHPUnit feature that
+		 * calls $app->bootstrap. @todo: link to documentation. */
+		$this->bootstrap = array ($app, 'bootstrap');
 		parent::setUp ();
 	}
 }
