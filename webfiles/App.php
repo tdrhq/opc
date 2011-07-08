de56767941bcf43e7e1f6eb879fb734aefe74e14
Anudhyan Boral <bloopsie@gmail.com>
2011-07-08 08:09:03 +0530

    Fixes URI parameter gathering in contests. This also fixes pagination in the queue.

--- webfiles/App.php
+++ webfiles/App.php
@@ -104,7 +104,7 @@ class App {
 		
 		$router = $frontController->getRouter(); // returns a rewrite router by default
 		
-		$router->addRoute ('comtestroute', new Zend_Controller_Router_Route ('contests/:contestid/:controller/:action', array ('controller' => 'index', 'action' => 'index')));
+		$router->addRoute ('contestroute', new Zend_Controller_Router_Route ('contests/:contestid/:controller/:action/*', array ('controller' => 'index', 'action' => 'index')));
 
 
 		/* for each of the following routes, we need two versions, with
